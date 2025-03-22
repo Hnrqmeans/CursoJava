27a1977d54599c236cf38db458479911cae73553
Henrique Minillo <henriqueminillo@hotmail.com>
2025-03-22 11:07:12 -0300

    Enviando Entrada de dados Scanner

---
+++ github/CursoJavaCompleto/Aulas/Basico/demo/src/test/java/EntradaDeDadosScanner02.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class EntradaDeDadosScanner02 {
+    public static void main(String[] args) {
+
+        //---------Resumo----------//
+
+        // Scanner --
+        // next()
+        // nextInt()
+        // nextDouble()
+        // next().charAt(0)
+
+        // Locale --
+        //  Quebra de linha
+        //  nextLine()
+        //  como limpar buffer de leitura
+
+        //-------------------------//
+        
+        // Scanner sc = new Scanner (System.in);
+
+        // int x;
+        // String s1, s2, s3;
+        // x = sc.nextInt();
+        // sc.nextLine();
+        // s1 = sc.nextLine();
+        // s2 = sc.nextLine();
+        // s3 = sc.nextLine();
+
+        // System.out.println("Dados Digitados:");
+        // System.out.println(x);
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(s3);
+
+        // sc.close();
+
+    }
+}
---
+++ github/CursoJavaCompleto/Aulas/Basico/demo/src/test/java/ExemplosDeConversao.java
@@ -0,0 +1,44 @@
+public class ExemplosDeConversao {
+ public static void main(String[] args) {
+
+//---------EXEMPLO1----------//
+    //double b, B, h, area;
+    //b= 6.0;
+    //B =8.0;
+    //h =5.0;
+
+    //area = (b+B) / 2.0 * h;
+    //System.out.println(area);
+
+//---------EXEMPLO2----------//
+    
+    //float b, B, h, area;  //Para fazer em float é necessario por o "f" na frente dos valores
+    //b= 6f;
+    //B =8f;
+    //h =5f;
+
+    //area = (b+B) / 2.0f * h;
+    //System.out.println(area);
+
+//---------EXEMPLO3----------//
+
+//    int a, b;
+//    double resultado;
+
+//    a =5;
+//    b =2;
+
+//    resultado = (double) a/b;  //(double)= isso se chama casting, necessario para conversão, já que a, b foram definidas como int.
+//    System.out.println(resultado);
+
+//---------EXEMPLO4----------//
+
+//    double a;
+//    int b;
+//    a =  5.0;
+//    b =  (int) a;   //necessario converter a para (int) pos a é um (double)
+
+//    System.out.println(b);
+
+ }
+}
